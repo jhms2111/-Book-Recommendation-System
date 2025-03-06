c8c64fccbeefb3a6773a7765aac6e2d1acad160f
Joao Henrique de Morais Serradourada <jhms211190@gmail.com>
2025-03-06 18:17:59 +0100

    Adjusting for render 1.

--- backend/src/infrastructure/auth/passport.js
+++ backend/src/infrastructure/auth/passport.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const User = require('../database/models/User');
+const User = require('../dataBase/User');
 require('dotenv').config();
 
 const credentials = {
