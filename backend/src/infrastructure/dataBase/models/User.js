1526161b888e2239ff5ced87e6f032907d2c6b16
Joao Henrique de Morais Serradourada <jhms211190@gmail.com>
2025-03-12 06:51:48 +0100

    Added admin role and authentication middleware, tested in production 11

--- backend/src/infrastructure/dataBase/models/User.js
+++ backend/src/infrastructure/dataBase/models/User.js
@@ -1,11 +1,11 @@
-// backend/models/User.js
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     senha: { type: String, required: false }, // A senha deve ser opcional
-    googleId: { type: String } // Para autenticação via Google
+    googleId: { type: String }, // Para autenticação via Google
+    role: { type: String, enum: ["admin", "user"], default: "user" } // 🔥 Agora com role
 });
 
 module.exports = mongoose.model('User', userSchema);
