f65ca32b19255c9a54cd6dd83c532aaeea41b457
Joao Henrique de Morais Serradourada <jhms211190@gmail.com>
2025-03-12 17:32:43 +0100

    Fix authentication loop and admin route protection 23

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,9 @@ import BookReviewPage from './pages/Books/BookReviewPage'; // Importe o componen
 import UserBooksPage from './pages/Books/UserBooksPage';
 import RankingPage from './pages/Ranking/RankingPage'
 import Feed from './components/Feed/Feed'; // Novo: componente para exibir o feed de postagens
+import AdminPage from './pages/Admin/AdminPage';
+import ProtectedAdminRoute from "./components/Auth/ProtectedAdminRoute";
+
 import Layout from './components/Header/Layout'; // Importa o Layout
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -54,6 +57,17 @@ const App = () => {
                         </ProtectedRoute>
                     }
                 />
+
+<Route
+    path="/admin"
+    element={
+        <ProtectedAdminRoute>
+            <Layout>
+                <AdminPage />
+            </Layout>
+        </ProtectedAdminRoute>
+    }
+/>
 
                 {/* Rota de sucesso após login com o Google */}
                 <Route path="/auth/success" element={<AuthSuccess setIsAuthenticated={setIsAuthenticated} />} />
