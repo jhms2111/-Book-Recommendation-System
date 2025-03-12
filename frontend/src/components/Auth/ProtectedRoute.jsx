e4c55270acbfd710b3bc3a3a26f06d8f8cd68be8
Joao Henrique de Morais Serradourada <jhms211190@gmail.com>
2025-03-12 16:24:52 +0100

    Fix authentication loop and admin route protection 17

--- frontend/src/components/Auth/ProtectedRoute.jsx
+++ frontend/src/components/Auth/ProtectedRoute.jsx
@@ -1,21 +1,13 @@
+// src/components/Auth/ProtectedRoute.jsx
+
 import { Navigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { useEffect, useState } from 'react';
 
 const ProtectedRoute = ({ children, isAuthenticated }) => {
-    const [hasRedirected, setHasRedirected] = useState(false);
-
-    useEffect(() => {
-        if (!isAuthenticated && !hasRedirected) {
-            setHasRedirected(true); // Marca que já houve redirecionamento
-        }
-    }, [isAuthenticated, hasRedirected]);
-
-    if (!isAuthenticated && !hasRedirected) {
+    if (!isAuthenticated) {
         console.log('Usuário não autenticado, redirecionando para login...');
-        return <Navigate to="/" replace />;
+        return <Navigate to="/login" replace/>;
     }
-
     console.log('Usuário autenticado, permitindo acesso à rota.');
     return children;
 };
