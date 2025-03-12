6e84e251dd32ff09b5ec1fc1b0b6a6ebab77c996
Joao Henrique de Morais Serradourada <jhms211190@gmail.com>
2025-03-12 14:41:45 +0100

    feat: add admin panel with protected route 11

--- frontend/src/components/Auth/ProtectedRoute.jsx
+++ frontend/src/components/Auth/ProtectedRoute.jsx
@@ -3,28 +3,26 @@ import PropTypes from "prop-types";
 import jwt_decode from "jwt-decode";
 
 const ProtectedRoute = ({ children, isAuthenticated }) => {
-    const location = useLocation(); // Obtém a rota atual de forma segura
+    const location = useLocation();
 
     if (!isAuthenticated) {
         console.log("Usuário não autenticado, redirecionando para login...");
         return <Navigate to="/login" />;
     }
 
-    // Verifica se o usuário tem permissão de admin
     const token = localStorage.getItem("token");
     if (token) {
         try {
             const decoded = jwt_decode(token);
             console.log("Usuário autenticado:", decoded);
 
-            // Se a rota for /admin e o usuário não for admin, redireciona para home
             if (location.pathname === "/admin" && decoded.role !== "admin") {
                 console.log("Acesso negado: usuário não é admin.");
                 return <Navigate to="/" />;
             }
         } catch (error) {
             console.error("Erro ao decodificar token:", error);
-            return <Navigate to="/" />; // Redireciona para home se o token for inválido
+            return <Navigate to="/" />;
         }
     }
 
@@ -33,7 +31,7 @@ const ProtectedRoute = ({ children, isAuthenticated }) => {
 
 ProtectedRoute.propTypes = {
     children: PropTypes.node.isRequired,
-    isAuthenticated: PropTypes.bool,
+    isAuthenticated: PropTypes.bool.isRequired,
 };
 
 export default ProtectedRoute;
